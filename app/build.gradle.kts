cbc796f0d24e424e9a2d5e7005353173534ed339
fellipito <fellipepereira0@gmail.com>
2025-07-13 14:51:14 -0300

    v0.8.8 - Conserto no path ao voltar de uma pasta se gura, melhoria de visual enquanto acrrega uma pasta 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 33
         targetSdk = 35
         versionCode = 1
-        versionName = "0.8.7"
+        versionName = "0.8.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
