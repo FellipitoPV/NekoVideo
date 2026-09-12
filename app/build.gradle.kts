7babb30bc04ed0682c1d1bee53f8d22bac09eb5b
fellipito <fellipepereira0@gmail.com>
2026-09-12 17:30:44 -0300

    feat: prepare 1.4.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nkls.nekovideo"
         minSdk = 30
         targetSdk = 36
-        versionCode = 37
-        versionName = "1.3.15"
+        versionCode = 38
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
